0811bca4338587dab6d855dc62cc3b25465a2329
ShannonKuo <b03901009@ntu.edu.tw>
2016-11-18 01:37:19 +0800

    Add files via upload

--- hw3/train.sh
+++ hw3/train.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#THEANO_FLAGS=mode=FAST_RUN,,device=gpu,floatX=float32 python selfTraining.py $1 $2
-#THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32,lib.cnmem=1 python selfTraining.py $1 $2
-python auto_encoder.py $1 $2
+python self_learning.py $1 $2
