3f9f054af5f71a27767cd5fbbfe2bf6eb8d8b74b
ShannonKuo <b03901009@ntu.edu.tw>
2016-11-18 00:49:08 +0800

    Delete train.sh

--- train.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#THEANO_FLAGS=mode=FAST_RUN,,device=gpu,floatX=float32 python selfTraining.py $1 $2
-#THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32,lib.cnmem=1 python selfTraining.py $1 $2
-python auto_encoder.py $1 $2
